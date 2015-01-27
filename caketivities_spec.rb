ad1e1bc7e33be367489885f8700b306177da7090
Tobias Pfeiffer <tobi@bitcrowd.net>
2015-01-27 21:21:26 +0100

    test for find with weird ifnone, and block

--- caketivities_spec.rb
+++ caketivities_spec.rb
@@ -18,17 +18,26 @@ describe "Caketivities" do
   before do
     @caketivities_array = []
     subject.each do |cake|
-    @caketivities_array << cake.activity
-  end
-
+     @caketivities_array << cake.activity
+    end
   end
 
   it "has all caketivities in an array" do
     expect(@caketivities_array).to eq ["bake", "decorate", "eat", "throw", "send", "sell", "gift"]
   end
 
-  it "finds the first activity which has more than 4 letters" do
-    @caketivities_array.find { |c| c.length > 4 }
-    expect(c.activity).to eq "decorate"
+  it "finds the first activity which has more than 3 letters" do
+    act = @caketivities_array.find { |c| c.length > 3 }
+    expect(act).to eq "bake"
+  end
+
+  it "returns nil when checking for something nonexistant" do
+    act = @caketivities_array.find { |c| c.length > 100 }
+    expect(act).to be_nil
+  end
+
+  it 'returns whatever ifnone is if it finds nothing' do
+    act = @caketivities_array.find(proc {'OH HAY TAM'}) { |c| c.length < 1}
+    expect(act).to eq 'OH HAY TAM'
   end
 end
