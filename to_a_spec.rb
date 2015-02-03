f48bc63095b354186f86bec6db5a38ee165418d3
Susanne Dewein <susanne.dewein@gmail.com>
2015-02-03 19:59:33 +0100

    add smoar specsand tobz example

--- to_a_spec.rb
+++ to_a_spec.rb
@@ -26,5 +26,23 @@ class Tam
       result = tarn_shoes.characteristics.to_a
       expect(result).to eq [[:obsessions, "kitchen towelz"], [:face, "dumb cake face"], [:intelligence, false]]      
     end
+
+    it "returns an array if called on an array" do
+      array = ["hallo", "rubycorns"]
+      result = array.to_a
+      expect(result).to eq ["hallo", "rubycorns"]
+    end
+
+    class MyArray
+      # normally that should be the representation of the object that makes sense 
+      def to_a
+        ['torn', 'shoes']
+      end
+    end
+
+    it "is used to convert custom objects to an array" do
+     object =  MyArray.new
+     expect(object.to_a).to eq ['torn', 'shoes']
+    end
   end
 end
