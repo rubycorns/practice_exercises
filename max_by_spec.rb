f6cc9aa93f31b33b60aaa9525ee76ca988256f33
Lisa Passing <mail@lislis.de>
2015-02-24 19:48:24 +0100

    fix the specs

--- max_by_spec.rb
+++ max_by_spec.rb
@@ -3,17 +3,18 @@ describe 'max_by' do
 
   sweet_stuff = ['cheesecake', 'lemon cake', 'almond tarte', 'blondies']
 
-  # it 'returns an enum when no block is given' do
-  #   expect(sweet_stuff.max_by).to eq sweet_stuff.max_by
-  # end
+  it 'returns an Enumerator when no block is given' do
+    expect(sweet_stuff.max_by).to be_an Enumerator
+  end
 
   it 'returns the maximum value from the given block' do
     expect(sweet_stuff.max_by { |nom| nom }).to eq 'lemon cake'
     expect(sweet_stuff.max_by { |nom| nom.length }).to eq 'almond tarte'
   end
 
-  # it 'takes an optional argument that determines how many values are returned ' do
-  #   expect(sweet_stuff.max_by(2) { |nom| nom }).to eq ['lemon cake', 'cheesecake']
-  # end
+  it 'takes an optional argument that determines how many values are returned ' do
+    # expect(sweet_stuff.max_by(2) { |nom| nom }).to eq ['lemon cake', 'cheesecake']
+    expect(sweet_stuff.sort.reverse.first(2)).to eq ['lemon cake', 'cheesecake']
+  end
 
 end
